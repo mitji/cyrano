4dfcded02a3b33ff6cafcc48763cb02cbfb70413
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2019-11-27 10:29:07 +0100

    updated seeds

--- bin/seedQuotes.js
+++ bin/seedQuotes.js
@@ -6,52 +6,368 @@ require('dotenv').config();
 
 const quotes = [
   [{
-    text: 'Tus imperfecciones te convierten en la mujer perfect',
-    //author: '5ddbba117c06d41722baa19f',
-    likes: [],
-    language: 'es',
-    type: ''
-  }, {
     text: 'I love you. Today. Right now. Just as you are.',
     //author: '5ddbba117c06d41722baa19f',
     likes: [],
-    language: 'es',
+    language: 'en',
     type: ''
-  }],
-  [{
-    text: 'Te haría mellizos hasta que salieran impares',
+  }, 
+  {
+    text: "Love is a dream that came to life when we meet",
     //author: '5ddbba117c06d41722baa19f',
     likes: [],
-    language: 'es',
+    language: 'en',
     type: ''
-  }],
-  [{
-    text: 'Tus ojos son como dos uvas, tu boca como una manzana. ¡Qúe buena ensalada de fruta haríamos con mi banana!',
+  },
+  {
+    text: "You don't marry someone you can live with ... you marry the person who you cannot live without.",
     //author: '5ddbba117c06d41722baa19f',
     likes: [],
-    language: 'es',
+    language: 'en',
     type: ''
-  }],
-  [{
-    text: 'No sé nada de amor, estoy casado',
+  },
+  {
+    text: 'Sometimes I can’t see myself when I’m with you. I can only just see you.',
     //author: '5ddbba117c06d41722baa19f',
     likes: [],
-    language: 'es',
+    language: 'en',
     type: ''
-  }, {
-    text: 'I love you more than I have ever found a way to say to you.',
+  },
+  {
+    text: "If we had just one cookie, I'd give you the big half. That's how much I love you.",
     //author: '5ddbba117c06d41722baa19f',
     likes: [],
-    language: 'es',
+    language: 'en',
     type: ''
-  }, {
-    text: 'Love is a dream that came to life when we meet',
-    //author: '5ddbba117c06d41722baa19f',
-    likes: [],
-    language: 'es',
-    type: ''
-  }],
-]
+  }
+], // 1 user
+[{
+  text: 'The luckiest day of my life was the day that I met you.',
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "I love being with you more than anything else in the world.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "I don't think I could ever love anyone else the way that I love you.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: 'Love You to the Moon and Back',
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "i wanna know what love is … I want you to show me.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 2 user
+[{
+  text: "Every day that we're together,  is a better day than the one before. I love you. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "With you is my very favorite place to be. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "There's no love like true love. There's no love like yours and mine.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "You're on my mind and in my heart.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "My night has become a sunny dawn because of you.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+],  // 3 user
+[{
+  text: "The water shines only by the sun. And it is you who are my sun. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "I know I am in love with you because my reality is finally better than my dreams. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "I will love you until the stars go out, and the tides no longer turn.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Every time I see you, I fall in love all over again.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Our Relationship is meant to be. Something that was written in the stars and drawn into our destiny.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 4 user
+
+[{
+  text: "The water shines only by the sun. And it is you who are my sun. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "I know I am in love with you because my reality is finally better than my dreams. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "I will love you until the stars go out, and the tides no longer turn.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Every time I see you, I fall in love all over again.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Our Relationship is meant to be. Something that was written in the stars and drawn into our destiny.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 5 user
+
+[{
+  text: "Just when I think that it is impossible to love you any more, you prove me wrong. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "I have loved you all my life; it has just taken me this long to find you. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "The best feeling is when you look at him…and he is already staring.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "To the world, you may be one person, but to one person you are the world.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Loving you never was an option. It was a necessity.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 6 user
+[{
+  text: "I love you more than I have ever found a way to say to you.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "It is true that my heart always skips a beat when you take my name.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Never above you. Never below you. Always beside you. ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Love every little thing about you.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "The thing I love about you is pretty much everything.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 7 user
+[{
+  text: "All I want is to love you and to wake up every morning with you by my side.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "You're all I need, and I simply want to tell you that I love you",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "I love you Today Tomorrow Always ",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "You're the better part of me that takes my whole world to a higher level. I love you",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "You have my heart… and you always will.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 8 user
+[{
+  text: "To love you is to have all that I need in the world",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "You hold my heart. Always have. Always will.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "You fill all my senses and bring magic into my life.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "I am happiest when I’m right next to you.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "I swear I couldn’t love you more than I do right now, and yet I know I will tomorrow.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+], // 9 user
+[{
+  text: "You may hold my hand for a while, but you hold my heart forever.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}, 
+{
+  text: "I need you like a heart needs a beat.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "Come live in my heart and pay no rent.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "If I know what love is, it is because of you.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+},
+{
+  text: "There are only two times that I want to be with you. Now and Forever.",
+  //author: '5ddbba117c06d41722baa19f',
+  likes: [],
+  language: 'en',
+  type: ''
+}
+] // 10 user
+]//end of quotes
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then( () => {
--- bin/seedUsers.js
+++ bin/seedUsers.js
@@ -8,45 +8,108 @@ require('dotenv').config();
 
 const users = [
   {
-    username: 'Erik',
-    email: 'erik@gmail.cat',
-    password: 'Erik',
-    pictureUrl: 'https://cdn5.vectorstock.com/i/1000x1000/23/49/new-man-avatar-icon-flat-vector-19152349.jpg',
-    bio: 'Tus imperfecciones te convierten en la mujer perfect',
+    username: 'John',
+    email: 'John@gmail.cat',
+    password: 'John',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845217/usersimages/ryxxlufshtlmhybuuh2e.jpg",
+    bio: 'I love you more than console.log',
     quotes: [],
     favorites: [],
     liked: [], 
   },
   {
-    username: 'David',
-    email: 'david@gmail.cat',
-    password: 'David',
-    pictureUrl: 'https://cdn5.vectorstock.com/i/1000x1000/23/49/new-man-avatar-icon-flat-vector-19152349.jpg',
-    bio: 'Te haría mellizos hasta que salieran impares',
+    username: 'Mary',
+    email: 'Mary@gmail.cat',
+    password: 'Mary',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845803/usersimages/idudv2tytcol0xnfrsaz.png",
+    bio: 'Live without you its like a lab without commits',
     quotes: [],
     favorites: [],
     liked: [], 
   },
   {
-    username: 'Rafa',
-    email: 'rafa@gmail.cat',
-    password: 'Rafa',
-    pictureUrl: 'https://cdn5.vectorstock.com/i/1000x1000/23/49/new-man-avatar-icon-flat-vector-19152349.jpg',
-    bio: 'Tus ojos son como dos uvas, tu boca como una manzana. ¡Qúe buena ensalada de fruta haríamos con mi banana!',
+    username: 'James',
+    email: 'James@gmail.cat',
+    password: 'James',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845217/usersimages/ryxxlufshtlmhybuuh2e.jpg",
+    bio: 'You make my hearth beat like getting 0 errors in hard katas',
     quotes: [],
     favorites: [],
     liked: [], 
   },
   {
-    username: 'Michal',
-    email: 'michal@gmail.cat',
-    password: 'Michal',
-    pictureUrl: 'https://cdn5.vectorstock.com/i/1000x1000/23/49/new-man-avatar-icon-flat-vector-19152349.jpg',
-    bio: 'No sé nada de amor, estoy casado',
+    username: 'Patricia',
+    email: 'Patricia@gmail.cat',
+    password: 'Patricia',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845803/usersimages/idudv2tytcol0xnfrsaz.png",
+    bio: "I can't live another day without git push",
     quotes: [],
     favorites: [],
     liked: [], 
   },
+  {
+    username: 'Robert',
+    email: 'Robert@gmail.cat',
+    password: 'Robert',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845217/usersimages/ryxxlufshtlmhybuuh2e.jpg",
+    bio: "All you need is undefined ",
+    quotes: [],
+    favorites: [],
+    liked: [], 
+  },
+  {
+    username: 'Linda',
+    email: 'Linda@gmail.cat',
+    password: 'Linda',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845803/usersimages/idudv2tytcol0xnfrsaz.png",
+    bio: 'Let me populate our collections',
+    quotes: [],
+    favorites: [],
+    liked: [], 
+  },
+  {
+    username: 'Michael',
+    email: 'Michael@gmail.cat',
+    password: 'Michael',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845217/usersimages/ryxxlufshtlmhybuuh2e.jpg",
+    bio: 'Git push it, baby',
+    quotes: [],
+    favorites: [],
+    liked: [], 
+  }, 
+  {
+    username: 'Jennifer',
+    email: 'Jennifer@gmail.cat',
+    password: 'Jennifer',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845803/usersimages/idudv2tytcol0xnfrsaz.png",
+    bio: "I'll be back",
+    quotes: [],
+    favorites: [],
+    liked: [], 
+  },
+  {
+    username: 'William',
+    email: 'William@gmail.cat',
+    password: 'William',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845217/usersimages/ryxxlufshtlmhybuuh2e.jpg",
+    bio: "Copy paste and success",
+    quotes: [],
+    favorites: [],
+    liked: [], 
+  },
+  {
+    username: 'Susan',
+    email: 'Susan@gmail.cat',
+    password: 'Susan',
+    pictureUrl: "https://res.cloudinary.com/cyranoapp/image/upload/v1574845803/usersimages/idudv2tytcol0xnfrsaz.png",
+    bio: "It must be a typo",
+    quotes: [],
+    favorites: [],
+    liked: [], 
+  },
+
+
+
 ]
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
@@ -54,7 +117,7 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
     const encrypUsers = users.map(user => {
       const salt = bcrypt.genSaltSync(saltRounds);
       user.password = bcrypt.hashSync(user.password,salt);
-      console.log(user.password);
+      //console.log(user.password);
       return user
     })
     return User.create(encrypUsers);
